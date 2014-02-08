6380adaf9855f81a3d40a736c4ee6aa70c09c736
Jeff Doyle (nfz) <shelleydoyle@eastlink.ca>
2014-02-08 16:10:40 -0400

    - compile renamed to docompile - docompile cannot use tail call optimization - compile, [compile], con, var, create are now in forth source

--- words/compiler.fs
+++ words/compiler.fs
@@ -23,3 +23,41 @@
     parse
     2drop
 ; immediate
+
+: [compile]
+  ' 0 cxt
+; immediate
+
+: compile
+  ['] docompile $0400 cxt
+  ' ,
+; immediate
+
+( x -- ) ( C: x "<spaces>name" -- )
+\ Compiler
+\ create a constant in the dictionary
+: con
+    rword
+    [compile] lit  
+    [compile] ret,
+;
+
+( cchar -- ) 
+\ Compiler
+\ create a dictionary entry for a variable and allocate 1 cell RAM
+: var
+    here
+    con
+    2
+    allot
+;
+
+( -- a-addr ) ( C: "<spaces>name" -- )
+\ Dictionary
+\ create a dictionary header. XT is (constant),
+\ with the address of the data field of name
+: create
+    rword
+    \ leave address after call on tos
+    compile popret
+;
