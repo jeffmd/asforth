f0d73b65497dc7e4a97645e69f23dbc90023c78e
Jeff Doyle (nfz) <shelleydoyle@eastlink.ca>
2014-04-06 11:36:15 -0300

    - allow inlinning of nop instruction - added time.fs which contains usec and msec

---
+++ words/time.fs
@@ -0,0 +1,22 @@
+( u -- )
+\ wait u microseconds
+: usec
+  2* \ the loop takes 8 clock cycles which is 1/2 microsecond
+  \ if cpu speed is 16MHZ
+  begin
+  ?while   \ 4 cycles
+  1-       \ 2 cycles
+  repeat   \ 2 cycles
+  drop
+;
+
+( u -- )
+\ wait u milliseconds
+: msec
+  begin
+  ?while
+  1-
+  1000 usec
+  repeat
+  drop
+;
