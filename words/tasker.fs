f1b51e34a21a07c3ca01ff346664297072bf7270
Jeff Doyle (nfz) <shelleydoyle@eastlink.ca>
2014-05-10 01:25:01 -0300

    - error in max tasks : only 31 not 32 - comments corrected

--- words/tasker.fs
+++ words/tasker.fs
@@ -3,20 +3,20 @@
 \ the active index into the task list
 cvar tidx
 
-\ count register for each task: max 32 tasks
-\ is an array of 32 bytes
+\ count register for each task: max 31 tasks
+\ is an array of 31 bytes
 cvar tcnt
-31 allot
+30 allot
 
 ( -- n )
 \ fetch task index: verifies index is valid
 \ adjusts index if count is odd ?
 : tidx@
   tidx c@ 
-  \ verify index is below 32
-  dup 31 >
+  \ verify index is below 31
+  dup 30 >
   if
-    \ greater than 31 so 0
+    \ greater than 30 so 0
     0:
     dup tidx c!
   then
@@ -35,9 +35,15 @@ cvar tcnt
   tcnt + 1+c!
 ;
 
-\ array of task slots in eeprom : max 32 tasks 64 bytes
+\ array of task slots in eeprom : max 31 tasks 62 bytes
+\ array is a binary process tree
+\                        0
+\             1                      2
+\      3           4           5           6
+\   7     8     9    10     11   12     13   14
+\ 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
 edp con tasks
-edp 64 + to edp
+edp 62 + to edp
 
 ( -- )
 \ increment task index to next task idx
@@ -58,7 +64,7 @@ edp 64 + to edp
 
 ( idx -- ) ( C:task_name )
 \ store a task in a slot
-\ idx is the slot index range: 0 to 31
+\ idx is the slot index range: 0 to 30
 : task
   2*
   tasks +
